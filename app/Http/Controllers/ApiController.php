0cf56b9c70e78cecd99eb1935cc0f2d8ef2979ed
Tangsholpan Abdipaiyz <190103356@stu.sdu.edu.kz>
2021-03-22 10:12:59 +0600

    LAB7 THE LAST COMMIT

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,7 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Post;
 class ApiController extends Controller
 {
     //
+    public function index(Request $request){
+    $post = Post::all();
+    return response($post,200);
+    }
+
+    public function get_post(Request $request){
+    $posts = Post::find($request->id);
+
+    if($posts == null){
+    return response(['message' => 'There is no post'] , 404);
+    }
+    return response($posts,200);
+    }
 }
